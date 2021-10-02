b8fc91cf77c21e3baff18f4c63bfff6c89e5d325
Pratush Bhandari <pratush.bh@gmail.com>
2021-10-02 12:22:33 +0530

    Add login route

--- server/router/auth.js
+++ server/router/auth.js
@@ -73,6 +73,39 @@ router.post('/register', async (req, res) => {
 
 })
 
+//login route
+
+router.post('/signin', async (req, res) => {
+    //console.log(req.body);
+
+    //res.json({ message: "this is the message"})
+
+    try{
+        const {email, password} = req.body;
+
+        if(!email || !password) {
+            return res.status(400).json({error: "Please fill the required data"})
+        } 
+
+        const userLogin = await User.findOne({email: email});
+
+        console.log(userLogin);
+
+        if(!userLogin) {
+            res.status(400).json({ error: "user error"});
+        } else{
+            res.json({message: "User login successful"});
+        }
+
+        
+
+
+    }catch (err) {
+        console.log(err);
+    }
+
+})
+
 
 
 module.exports = router;

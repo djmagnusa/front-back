5975a61a5f38b943f2c83e607b74f2d2fb5441d0
Pratush Bhandari <pratush.bh@gmail.com>
2021-10-03 19:04:04 +0530

    Update userSchema

--- server/app.js
+++ server/app.js
@@ -2,10 +2,12 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+const cookieparser = require("cookie-parser");
 
 dotenv.config({path: './config.env'});
 require('./db/conn');
 
+app.use(cookieparser());
 app.use(express.json());
 
 
@@ -16,10 +18,10 @@ app.use(require('./router/auth'));
 const PORT = process.env.PORT;
 
 
-const middleware = (req, res, next) => {
-    console.log('This is middleware');
-    next();
-}
+// const middleware = (req, res, next) => {
+//     console.log('This is middleware');
+//     next();
+// }
 
 
 // app.get('/', (req, res) => {
@@ -30,9 +32,9 @@ app.get('/signup', (req, res) => {
     res.send('Response from signup');
 })
 
-app.get('/signin', (req, res) => {
-    res.send('Hello from signin');
-})
+// app.get('/signin', (req, res) => {
+//     res.send('Hello from signin');
+// })
 
 app.listen(PORT, () => {
     console.log(`server is running at port ${PORT}`)
